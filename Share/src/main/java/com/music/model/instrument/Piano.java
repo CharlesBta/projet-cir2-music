843aaf166a210e26aeceff34a0aedab92253943b
Charles-Antoine <charto.bta@gmail.com>
2025-01-31 16:56:19 +0100

    init project MVC

---
+++ Share/src/main/java/com/music/model/instrument/Piano.java
@@ -0,0 +1,4 @@
+package com.music.model.instrument;
+
+public class Piano implements IInstrument{
+}
---
+++ Share/src/main/java/com/music/model/instrument/Xylophone.java
@@ -0,0 +1,4 @@
+package com.music.model.instrument;
+
+public class Xylophone implements IInstrument{
+}
