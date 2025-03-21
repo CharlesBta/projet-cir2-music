2196354ce2752303d5ab584ddb34184b71272e67
Charles-Antoine <charto.bta@gmail.com>
2025-03-21 14:26:45 +0100

    -

--- Share/src/main/java/com/music/InstrumentFactory.java
+++ Share/src/main/java/com/music/InstrumentFactory.java
@@ -3,7 +3,6 @@ package com.music;
 import com.music.model.instrument.IInstrument;
 import com.music.model.instrument.Synthesizer;
 import com.music.model.instrument.VideoGame;
-import com.music.model.instrument.Xylophone;
 
 public abstract class InstrumentFactory {
     public static IInstrument getInstrument(String instrument) {
@@ -12,7 +11,7 @@ public abstract class InstrumentFactory {
 
     public static IInstrument getInstrument(String instrument, Integer velocity) {
         return switch (instrument) {
-            case "Xylophone" -> new Xylophone();
+            case "Xylophone" -> new Synthesizer(velocity, 13);
             case "Piano" -> new Synthesizer(velocity, 0);
             case "Acoustic Guitar" -> new Synthesizer(velocity, 24);
             case "Electric Bass" -> new Synthesizer(velocity, 33);
