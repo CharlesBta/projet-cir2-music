bfd1b422450a84d70070cceb41f9e507a7c47533
Charles-Antoine <charto.bta@gmail.com>
2025-04-01 14:24:24 +0200

    Add piano functionality with dynamic key panels and controller integration

--- Main/src/main/java/com/music/Main.java
+++ Main/src/main/java/com/music/Main.java
@@ -1,7 +1,21 @@
 package com.music;
 
 
+import com.music.controller.Controller;
+import com.music.model.Model;
+import com.music.view.ConverterTxtToJson;
+import com.music.view.PianoView;
+import com.music.view.ReaderJson;
+
 public class Main {
     public static void main(String[] args) {
+        Model model = new Model();
+        Controller controller = new Controller(model);
+
+        controller.setInstrument("Piano");
+
+        PianoView pianoView = new PianoView(controller);
+        pianoView.setVisible(true);
+
     }
 }
