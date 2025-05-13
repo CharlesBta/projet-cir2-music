e2c60c8d7fae3805c9633419013694b500808eb3
BL4STORK <144135621+BL4STORK@users.noreply.github.com>
2025-05-13 14:12:43 +0200

    Refactor instrument panel detection and extend note offsets
    Simplified instrument panel detection logic in Frame.java for readability. Extended NoteOffsetCalculator to support "Wood" notes, ensuring compatibility with new instrument types. This refactor improves maintainability and prepares for future feature expansions.

--- Share/src/main/java/com/music/NoteOffsetCalculator.java
+++ Share/src/main/java/com/music/NoteOffsetCalculator.java
@@ -4,26 +4,26 @@ public abstract class NoteOffsetCalculator {
     public static Integer convertToOffset(String note) {
         return switch (note) {
 
-            case "Bit1","Bit8","Do", "B#" -> 0;  // B# est enharmonique de C/Do
+            case "Bit1","Wood1","Bit8","Do", "B#" -> 0;  // B# est enharmonique de C/Do
             case "Do#", "Ré♭", "C#", "Db" -> 1;
 
-            case "Bit2","Ré", "D" -> 2;
+            case "Bit2","Wood2","Ré", "D" -> 2;
             case "Ré#", "Mi♭", "D#", "Eb" -> 3;
 
-            case "Bit3","Mi", "E", "Fa♭", "Fb" -> 4;  // Fa♭ (Fb) est enharmonique de Mi
+            case "Bit3","Wood3","Mi", "E", "Fa♭", "Fb" -> 4;  // Fa♭ (Fb) est enharmonique de Mi
             case "Mi#" -> 5;  // Mi# (inhabituel) est enharmonique de Fa
 
-            case "Bit4","Fa", "F" -> 5;
+            case "Bit4","Wood4","Fa", "F" -> 5;
             case "Fa#", "Sol♭", "F#", "Gb" -> 6;
 
-            case "Bit5","Sol", "G" -> 7;
+            case "Bit5","Wood5","Sol", "G" -> 7;
             case "Sol#", "La♭", "G#", "Ab" -> 8;
 
-            case "Bit6","La", "A" -> 9;
+            case "Bit6","Wood6","La", "A" -> 9;
             case "La#", "Si♭", "A#", "Bb" -> 10;
 
-            case "Bit7","Si", "B", "Do♭", "Cb" -> 11;  // Do♭ (Cb) est l'équivalent de Si
-            case "Si#", "C" -> 0;  // Si# est enharmonique de Do
+            case "Bit7","Wood7","Si", "B", "Do♭", "Cb" -> 11;  // Do♭ (Cb) est l'équivalent de Si
+            case "Wood8","Si#", "C" -> 0;  // Si# est enharmonique de Do
 
             default -> null; // Valeur par défaut pour les entrées invalides
         };
