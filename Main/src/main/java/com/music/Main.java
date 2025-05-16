0cb46402337a59c3099dcd660c651b713d75e9af
Charles-Antoine <charto.bta@gmail.com>
2025-05-16 11:42:07 +0200

    Add Maven assembly plugin configuration for executable JAR creation package the project as a JAR with dependencies, specifying `com.music.Main` as the main class. Unused imports in Main.java are removed for better code readability and maintenance.

--- Main/src/main/java/com/music/Main.java
+++ Main/src/main/java/com/music/Main.java
@@ -1,15 +1,8 @@
 package com.music;
 
-import com.music.Note;
 import com.music.controller.Controller;
-import com.music.controller.IController;
 import com.music.model.Model;
 import com.music.view.Frame;
-import com.music.view.Record;
-
-import javax.swing.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class Main {
     public static void main(String[] args) {
